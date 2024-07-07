518d0b05172945744050f72be8a29cbfd67c54bb
carlos <carlosjperazab@gmail.com>
2024-07-07 14:02:18 -0400

    Cambiamos la conf del servidor app.js para que funcione la cabecera cors

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 import express from 'express';
 import { rutas } from './src/routers/routers.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+//Settings o Configuraciones
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.use(rutas);
 
--- src/routers/routers.js
+++ src/routers/routers.js
@@ -8,8 +8,4 @@ rutas.get('/', (req, res) => {
     res.json({ message: 'Hola, mundo!' });
 });
 
-rutas.get('/api', (req, res) => {
-    res.json({ message: 'Hola, nueva ruta!' });
-});
-
 rutas.post('/api/registrar-usuario', UsuarioControlador.registrarUsuario);
