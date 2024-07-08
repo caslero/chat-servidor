2f01c1637983367d3ffd4c7058ee4da6ca4a5e1f
carlos <carlosjperazab@gmail.com>
2024-07-08 12:07:52 -0400

    Hemos creado una db en vercel, cofig y agregado un usuario

--- src/database/db.js
+++ src/database/db.js
@@ -1,16 +1,20 @@
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
+import pg from 'pg';
 
 dotenv.config();
 
 // Configuración de la conexión a la base de datos
-export const sequelize = new Sequelize("postgres://default:URNVFZ6i2tYQ@ep-tiny-glitter-a44px9tx-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require", {
+// Configuración de la conexión a la base de datos
+export const sequelize = new Sequelize(process.env.POSTGRES_URL, {
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false
     }
-  }
+  },
+  dialect: 'postgres', // Agrega esta línea
+  dialectModule: pg // Agrega esta línea
 });
 
 export const User = sequelize.define('User', {
